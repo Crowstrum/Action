6f4bfec706bd507e7ce0efd831a08fa2329b3da7
crowstrum <crowstrum@gmail.com>
2015-06-02 13:26:58 -0500

    Initial
    for unity cloud

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+	public GameObject target;
+	private Vector3 _myPosition;
+	private Vector3 _targetPosition;
+	private bool _lerpPosition;
+	// Use this for initialization
+	void Start ()
+	{
+		_myPosition = transform.position;
+		_targetPosition = target.transform.position;
+		_lerpPosition = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+
+		transform.position = Vector3.Lerp (new Vector3 (transform.position.x, transform.position.y, transform.position.z),
+		                                   new Vector3 (target.transform.position.x, transform.position.y, target.transform.position.z), Time.deltaTime * 10f);
+
+	
+	}
+}
